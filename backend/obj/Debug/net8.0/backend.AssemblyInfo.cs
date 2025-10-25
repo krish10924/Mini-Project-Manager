c8319ae929e0fc32a52e35232a4b8c32b3b819df
Krishna Aggarwal <krishna10924@gmail.com>
2025-10-25 14:19:14 +0530

    Frontend removed

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+070784847657c2a20f7054ad02845fcc13fb3b92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f748aa7e691c0d6c8a6e32846a7ccb7ea63a1b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
